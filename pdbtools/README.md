5bb7cd9b1f3bb44e7b095363024eaef013326a03
Doruk Alp <dorukalpuzunarslan@gmail.com>
2023-06-12 18:47:22 +0300

    Radical change in class system

---
+++ pdbtools/README.md
@@ -0,0 +1,36 @@
+# PDBTOOLS
+
+![Author](https://img.shields.io/badge/Author-UDoruk3250-brightgreen)
+![Version](https://img.shields.io/badge/Version-0.1.2-red)
+
+A new python library to process .pdb files easier!
+
+* Reads, processes .pdb files and outputs the files easily and fast.
+
+
+* Send me an e-mail through [duzunarslan27@my.uaa.k12.tr](https://mail.google.com/mail/u/0/?ogbl#inbox) if you find anything wrong, deficit, or inadequate. I would be happy to help people. 
+
+
+***
+## How to use:
+
+````
+pip install pdbtools
+````
+```
+import pdbtools
+```
+
+
+You first need to install the package via pip. In later
+versions, the number of the platforms will be increased.
+Then you should be ready to use this library.
+***
+## Structure:
+As this is my first python package, I tried to keep 
+things simple and consize.
+>pdbtools
+> 
+> > <details><summary> pdbreader.py </summary> <ul> <li> importMolecule() <li>getIndexAtom() <li>PDB2FASTA()</li> </ul> </ul> </details>
+
+> 
--- pdbtools/pdbreader.py
+++ pdbtools/pdbreader.py
@@ -1,66 +1,68 @@
-class MoleculeStructure:
-    def __init__(self):
-        self.command = ""
-        self.atomlist = []
-        self.commandlist = ["HEADER", "ATOM  ", "TER   ", "END   ", "CRYST1", "TITLE ", "REMARK", "HETATM", "COMPND",
-                            "SOURCE", "KEYWDS", "EXPDTA", "AUTHOR", "REVDAT", "JRNL  ", "SEQRES", "SEQADV", "DBREF ",
-                            "HET   ", "HETNAM", "HETSYN", "FORMUL", "HELIX ", "SHEET ", "SSBOND", "LINK  ", "CISPEP",
-                            "ORIGX1", "ORIGX2", "ORIGX3", "SCALE1", "SCALE2", "SCALE3", "ANISOU", "CONECT", "MASTER"]
-        self.aminoacids = {"ALA": "A", "ASX": "B", "CYS": "C", "ASP": "D", "GLU": "E", "PHE": "F", "GLY": "G",
-                           "HIS": "H", "ILE": "I", "LYS": "K", "LEU": "L", "MET": "M", "ASN": "N", "PRO": "P",
-                           "GLN": "Q", "ARG": "R", "SER": "S", "THR": "T", "VAL": "V", "TRP": "W", "TYR": "Y",
-                           "GLX": "Z"}
+command = ""
+atomlist = []
+commandlist = ["HEADER", "ATOM  ", "TER   ", "END   ", "CRYST1", "TITLE ", "REMARK", "HETATM", "COMPND",
+               "SOURCE", "KEYWDS", "EXPDTA", "AUTHOR", "REVDAT", "JRNL  ", "SEQRES", "SEQADV", "DBREF ",
+               "HET   ", "HETNAM", "HETSYN", "FORMUL", "HELIX ", "SHEET ", "SSBOND", "LINK  ", "CISPEP",
+               "ORIGX1", "ORIGX2", "ORIGX3", "SCALE1", "SCALE2", "SCALE3", "ANISOU", "CONECT", "MASTER"]
 
-    def importMolecule(self, file=""):
-        if not file.endswith(".pdb"):
-            raise InvalidFileException
-        else:
-            try:
-                with open(file) as f:
-                    liste = list(f.read().split("\n"))
-                    for i, element in enumerate(liste):
+aminoacids = {"ALA": "A", "ASX": "B", "CYS": "C", "ASP": "D", "GLU": "E", "PHE": "F", "GLY": "G",
+              "HIS": "H", "ILE": "I", "LYS": "K", "LEU": "L", "MET": "M", "ASN": "N", "PRO": "P",
+              "GLN": "Q", "ARG": "R", "SER": "S", "THR": "T", "VAL": "V", "TRP": "W", "TYR": "Y",
+              "GLX": "Z"}
 
-                        if not (element.startswith("REMARK") or element.startswith("HEADER") or element.startswith(
-                                "TITLE")):
-                            self.atomlist.append(list(element.split()))
-                            if element.startswith("END"):
-                                break
-                            elif len(str(self.atomlist[-1][0])) > 6:
-                                self.atomlist[-1].insert(1, self.atomlist[-1][0][6:])
-                                self.atomlist[-1][0] = self.atomlist[-1][0][:6]
-                        else:
-                            i -= 1
-            except Exception:
-                raise IllegiblePDBFileException
 
-    def getIndexAtom(self, index=1):
-        if self.atomlist:
-            a = 0
-            s = 0
-            while s < index:
-                if self.atomlist[a][0] == "ATOM":
-                    s += 1
-                a += 1
+def importMolecule(file=""):
+    if not file.endswith(".pdb"):
+        raise InvalidFileException
+    else:
+        try:
+            with open(file) as f:
+                liste = list(f.read().split("\n"))
+                for i, element in enumerate(liste):
 
-        else:
-            raise NoSourceFileProvidedException
+                    if not (element.startswith("REMARK") or element.startswith("HEADER") or element.startswith(
+                            "TITLE")):
+                        atomlist.append(list(element.split()))
+                        if element.startswith("END"):
+                            break
+                        elif len(str(atomlist[-1][0])) > 6:
+                            atomlist[-1].insert(1, atomlist[-1][0][6:])
+                            atomlist[-1][0] = atomlist[-1][0][:6]
+                    else:
+                        i -= 1
+        except Exception:
+            raise IllegiblePDBFileException
 
-        return self.atomlist[a-1]
 
-    def PDB2FASTA(self):
-        fasta = ""
-        chain = ""
-        for line in self.atomlist:
-            if line[0] == "SEQRES":
-                if line[2] != chain:
-                    chain = line[2]
-                    fasta += "\n"
-                    fasta += "> Chain " + chain + ": \n  "
+def getIndexAtom(index=1):
+    if atomlist is not None:
+        a = 0
+        s = 0
+        while s < index:
+            if atomlist[a][0] == "ATOM":
+                s += 1
+            a += 1
 
-                for acid in line[4:]:
-                    fasta += self.aminoacids.get(str(acid))
+    else:
+        raise NoSourceFileProvidedException
 
-        return fasta
+    return atomlist[a - 1]
+
+
+def PDB2FASTA():
+    fasta = ""
+    chain = ""
+    for line in atomlist:
+        if line[0] == "SEQRES":
+            if line[2] != chain:
+                chain = line[2]
+                fasta += "\n"
+                fasta += "> Chain " + chain + ": \n  "
+
+            for acid in line[4:]:
+                fasta += aminoacids.get(str(acid))
+
+    return fasta
 
 
 class IllegiblePDBFileException(Exception):
