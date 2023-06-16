b3e9b79cb565a8c128c949917f43132044cd292d
Doruk Alp <dorukalpuzunarslan@gmail.com>
2023-06-16 17:54:58 +0300

    Update pdbvisualizer.py

--- pdbtools/pdbvisualizer.py
+++ pdbtools/pdbvisualizer.py
@@ -6,16 +6,11 @@ try:
 except Exception:
     os.system("pip install nglview")
 
-
-
 def show():
 
     # print(name.upper())
     print("-------------------")
     # n.show_url("https://swissmodel.expasy.org/templates/"+self.name.upper())
-
-PDBreader.show = show
-
 # class Visual(PDBreader):
 #     def __init__(self):
 #         super().__init__()
