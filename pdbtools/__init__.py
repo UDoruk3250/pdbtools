69449f666f36cb765612369dd943b8f86fcef6cb
Doruk Alp <dorukalpuzunarslan@gmail.com>
2023-06-18 12:10:03 +0300

    Update __init__.py

--- pdbtools/__init__.py
+++ pdbtools/__init__.py
@@ -1,6 +1,12 @@
 try:
+<<<<<<< HEAD
     from .pdbtool import *
     from .pdbreader import *
     from .pdbvisualizer import *
+=======
+    from pdbtools.pdbtoolkit.pdbtool import *
+    from pdbtools.pdbtoolkit.pdbreader import *
+    from pdbtools import *
+>>>>>>> visualizer
 except Exception:
     raise ImportError
